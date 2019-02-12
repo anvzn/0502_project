8432e77cc2295a80942b2c187e6c752a25a1b63c
leon740gk <leon740gk@gmail.com>
2019-02-12 21:09:41 +0200

    my_second_commit

--- test_74.py
+++ test_74.py
@@ -4,6 +4,12 @@ while True:
 
     user_ssn_number = input('Enter your SSN number in format (0000): ')
 
+
+    # check the length
     if len(user_ssn_number) != 4:
         print('Invalid SSN, please check the format (0000)!!!')
+        continue
+
+    if not user_ssn_number.isdigit():
+        print('Please enter digits only!!!')
         continue
