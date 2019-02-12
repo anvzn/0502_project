aacde3ab3e8601a8a49fee2517f10a6d814345f0
leon740gk <leon740gk@gmail.com>
2019-02-12 21:04:11 +0200

    my_first_commit

---
+++ test_74.py
@@ -0,0 +1,9 @@
+SSN_white_list = [1324, 5678, 9101]
+
+while True:
+
+    user_ssn_number = input('Enter your SSN number in format (0000): ')
+
+    if len(user_ssn_number) != 4:
+        print('Invalid SSN, please check the format (0000)!!!')
+        continue
